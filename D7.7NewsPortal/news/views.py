4eae7c9c58cf104867a7fc4972830e46a3f28fee
Veseya <155951956+Veseya@users.noreply.github.com>
2024-04-06 10:45:55 +0300

    Add files via upload

---
+++ D7.7NewsPortal/news/views.py
@@ -0,0 +1,70 @@
+from django.urls import reverse_lazy
+from datetime import datetime
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+
+
+class PostList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+    def get_queryset(self):
+        return Post.objects.order_by("-date_in")
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news_id.html'
+    context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if self.request.path == '/articles/create/':
+            post.post_type = "AR"
+        post.save()
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('news')
