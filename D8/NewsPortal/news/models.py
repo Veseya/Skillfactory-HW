104f65c86448e841df98e2e00d6c96ace3bf30f3
Veseya <155951956+Veseya@users.noreply.github.com>
2024-04-10 17:46:48 +0300

    Add files via upload

---
+++ D8/NewsPortal/news/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        posts_rating = 0
+        comments_rating = 0
+        posts_comments_rating = 0
+        posts = Post.objects.filter(author=self)
+        for p in posts:
+            posts_rating += p.rating
+
+        comments = Comment.objects.filter(user=self.user)
+        for c in comments:
+            comments_rating += c.rating
+
+        posts_comments = Comment.objects.filter(post__author=self)
+        for d in posts_comments:
+            posts_comments_rating += d.rating
+
+        self.rating = posts_rating * 3 + comments_rating + posts_comments_rating
+        self.save()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name.title()
+
+
+class Post(models.Model):
+
+    news = "NW"
+    articles = "AR"
+
+    POST_TYPES = [
+        (news, "Новость"),
+        (articles, "Статья")
+    ]
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=2, choices=POST_TYPES, default=news)
+    date_in = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through="PostCategory")
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return f"{self.text[:124]}..."
+
+    def __str__(self):
+        return f'{self.title.title()}: {self.text[:20]}'
+
+    def get_absolute_url(self):
+        return reverse('news_detail', args=[str(self.id)])
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_in = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
